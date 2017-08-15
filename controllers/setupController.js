20c0fbcc4ff939838e6eca3cdad589a4305a9586
Imran Panjwani <imranpanjwani00@gmail.com>
2017-08-16 00:55:44 +0530

    create setup controller

---
+++ controllers/setupController.js
@@ -0,0 +1,31 @@
+var Todo = require('../models/todoModel');
+
+module.exports = function(app){
+    app.get('/api/setupTodo',function(req,res){
+
+        var todos = [
+            {
+                username:'rajesh',
+                todo:'Buy shoes',
+                isDone:false,
+                hasAttachment:false
+            },
+            {
+                username:'rajesh',
+                todo:'Learn MEAN',
+                isDone:false,
+                hasAttachment:false
+            },
+            {
+                username:'rajesh',
+                todo:'Complete assignment',
+                isDone:false,
+                hasAttachment:false
+            }
+        ];
+        Todo.create(todos,function(err,results){
+            res.send(results);
+        });
+
+    });
+}
